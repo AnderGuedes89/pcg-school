7bf37a9bd6f595778e694fe023bce30ab581ad64
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-11-23 17:44:38 -0300

    Simples layout na tela Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoaderService } from './shared/services/loader.service';
 
 @Component({
--- src/app/layouts/pages/master-page/master-page.component.ts
+++ src/app/layouts/pages/master-page/master-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-master-page',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./master-page.component.scss']
 })
 export class MasterPageComponent implements OnInit {
-
-  constructor() { }
+  router: string;
+  constructor(private _router: Router) {    this.router = _router.url; }
 
   ngOnInit(): void {
   }
